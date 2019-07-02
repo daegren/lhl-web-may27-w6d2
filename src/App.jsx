c7f88311073586a11bcff08bbd7e12bc3b4ef745
David Mills <dave@1morepx.com>
2019-07-02 13:03:56 -0400

    1. Prototype the UI in an existing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <h1>Hello React :)</h1>
+        <div className="counter">
+          <button>-</button>
+          <span className="value">{0}</span>
+          <button>+</button>
+        </div>
+      </div>
     );
   }
 }
