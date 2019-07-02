26e964efd8c9092d337c31c615fa5b263fd0e293
David Mills <dave@1morepx.com>
2019-07-02 13:14:53 -0400

    2. Make the prototype work functionally.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { counter: 0 };
+  }
+
   render() {
     return (
       <div>
         <h1>Hello React :)</h1>
         <div className="counter">
-          <button>-</button>
-          <span className="value">{0}</span>
-          <button>+</button>
+          <button onClick={this._handleDecrement}>-</button>
+          <span className="value">{this.state.counter}</span>
+          <button onClick={this._handleIncrement}>+</button>
         </div>
       </div>
     );
   }
+
+  _handleIncrement = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  _handleDecrement = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
 }
 export default App;
